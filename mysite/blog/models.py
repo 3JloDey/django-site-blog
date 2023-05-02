61c8b0a86f1395981e12cce3573fed9768f5c72d
3JloDey <aleksey.smolensky67@gmail.com>
2023-05-02 22:11:36 +0300

    improved urls and added absolute url

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -14,7 +15,7 @@ class Post(models.Model):
         PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
@@ -32,6 +33,12 @@ class Post(models.Model):
     class Meta:
         ordering = ["-publish"]
         indexes = [models.Index(fields=["-publish"])]
+
+    def get_absolute_url(self):
+        return reverse(
+            "blog:post_detail",
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug],
+        )
 
     def __str__(self) -> str:
         return self.title
