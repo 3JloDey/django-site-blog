05011112bbb02b8203f7b91ada9c9ab4c22cccc2
3JloDey <118563293+3JloDey@users.noreply.github.com>
2023-07-06 18:12:27 +0300

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "blog",
         "USER": "postgres",
-        "PASSWORD": "Smol_Ros67Aleksey_RUS"
+        "PASSWORD": ""
     }
 }
 
